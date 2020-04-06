d19c8d3e7ea6c0c0a592151e3bb6bf9c12a5541f
yoondayoung <agwin73@naver.com>
2020-04-06 21:29:15 +0900

    first commit

---
+++ myproject/wordcount/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'wordcount/home.html')
+
+def about(request):
+    return render(request, 'wordcount/about.html')
+
+def result(request):
+    text = request.GET['fulltext']
+    words = text.split() #띄어쓰기로 구분
+    word_dic = {}
+
+    for x in words:
+        if x in word_dic:
+            word_dic[x] += 1
+        else:
+            word_dic[x] = 1    
+
+    return render(request, 'wordcount/result.html', {'total':text, 'dic':word_dic.items()})
